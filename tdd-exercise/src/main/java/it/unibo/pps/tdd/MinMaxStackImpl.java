428d2f0c05b3599f7baac2f53148bdb3171e2885
Luca Marchi <luca1010.marchi@gmail.com>
2026-02-25 16:29:21 +0100

    test min max

--- tdd-exercise/src/main/java/it/unibo/pps/tdd/MinMaxStackImpl.java
+++ tdd-exercise/src/main/java/it/unibo/pps/tdd/MinMaxStackImpl.java
@@ -3,41 +3,56 @@ package it.unibo.pps.tdd;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Integer.*;
+
 public class MinMaxStackImpl implements MinMaxStack{
     private final List<Integer> stack = new ArrayList<>();
+    private int minValue = MAX_VALUE;
+    private int maxValue = MIN_VALUE;
+
 
     @Override
     public void push(int value) {
         stack.add(value);
+        this.minValue = min(minValue, value);
+        this.maxValue = max(maxValue, value);
     }
 
     @Override
     public int pop() {
-        return 0;
+        if (stack.isEmpty()){
+            throw new IllegalStateException("Stack is empty!");
+        }
+        int value = stack.getLast();
+        stack.removeLast();
+        return value;
     }
 
     @Override
     public int peek() {
+        if (stack.isEmpty()){
+            throw new IllegalStateException("Stack is empty");
+        }
         return stack.getLast();
     }
 
     @Override
     public int getMin() {
-        return 0;
+        return this.minValue;
     }
 
     @Override
     public int getMax() {
-        return 0;
+        return this.maxValue;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return stack.isEmpty();
     }
 
     @Override
     public int size() {
-        return 0;
+        return stack.size();
     }
 }
--- tdd-exercise/src/test/java/it/unibo/pps/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/it/unibo/pps/tdd/MinMaxStackImplTest.java
@@ -3,23 +3,69 @@ package it.unibo.pps.tdd;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+    private MinMaxStack minMaxStack;
+    private static final int VALUE = 1;
+    private static final int MINIMUM_VALUE = -10;
+    private static final int MAXIMUM_VALUE = 10;
+
     @BeforeEach
     public void beforeEach(){
-
+        minMaxStack = new MinMaxStackImpl();
     }
 
     @Test
-    public void testPush() {
-        final MinMaxStack stack = new MinMaxStackImpl();
-        stack.push(1);
-        assertEquals(1, stack.peek());
-
+    public void testPush(){
+        minMaxStack.push(VALUE);
+        assertFalse(minMaxStack.isEmpty());
     }
+
+    @Test
+    public void testPeek() {
+        minMaxStack.push(VALUE);
+        assertEquals(VALUE, minMaxStack.peek());
+    }
+
+    @Test
+    public void testPeekWithEmptyStack(){
+        assertThrows(IllegalStateException.class, () -> minMaxStack.peek());
+    }
+
+    @Test
+    public void testPopValue(){
+        minMaxStack.push(VALUE);
+        assertEquals(VALUE, minMaxStack.pop());
+    }
+
+    @Test
+    public void testPopRemoval(){
+        minMaxStack.push(VALUE);
+        minMaxStack.pop();
+        assertEquals(0, minMaxStack.size());
+    }
+
+    @Test
+    public void testPopWithEmptyStack(){
+        assertThrows(IllegalStateException.class, () -> minMaxStack.pop());
+    }
+
+    @Test
+    public void testGetMin(){
+        for (int i = MINIMUM_VALUE; i <= MAXIMUM_VALUE; i++) {
+            minMaxStack.push(i);
+        }
+        assertEquals(MINIMUM_VALUE, minMaxStack.getMin());
+    }
+
+    @Test
+    public void testGetMax(){
+        for (int i = MINIMUM_VALUE; i <= MAXIMUM_VALUE; i++){
+            minMaxStack.push(i);
+        }
+        assertEquals(MAXIMUM_VALUE, minMaxStack.getMax());
+    }
+
 
 }
